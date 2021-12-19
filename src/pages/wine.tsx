ad991138ac76a74bf0add99f9225f0562129d924
Kyoo130 <kml9003@kakao.com>
2021-12-19 22:30:26 +0900

    :sparkles: Wine 페이지 생성

---
+++ src/pages/wine.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const WinePage: NextPage = () => {
+  return (
+    <div>
+      <h2>여기는 wine 페이지 입니다.</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A nostrum aliquid molestiae tempora aspernatur accusamus corporis, vitae quibusdam doloribus voluptates inventore, veniam exercitationem, eaque maiores adipisci laudantium ut laborum sint.</p>
+    </div>
+  );
+};
+
+export default WinePage;
