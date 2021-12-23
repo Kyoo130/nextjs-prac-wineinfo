a404d55c6074c1cd92782fd27c11399fd3dd1ac8
Kyoo130 <kml9003@kakao.com>
2021-12-23 22:08:06 +0900

    ✨ Layout 구현

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -1,12 +1,11 @@
-import { Beer } from "../types/Beer";
+import { Beer } from "../types/Beer"
 
-interface BeerProps {
+interface BeerCardProps {
   beerData: Beer
 }
 
-export const BeerCard = ({ beerData }: BeerProps) => {
+export const BeerCard = ({ beerData }: BeerCardProps) => {
   const { name, price } = beerData;
-
   return (
     <div>
       <h2>{name}</h2>
